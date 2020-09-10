76a7a6c9d15bf3d1199cbc835331d7958e32c467
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-09-10 18:52:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,3 @@ color_list = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 def display_rainbow(color_list)
 puts "R: #{[0]}, O: #{[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]}"
 end
-
